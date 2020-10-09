e816a3bad4f6a57ed8d77b003820c45dea7645ca
Adam White <adamfrankwhite@gmail.com>
2020-10-09 23:11:54 +0100

    scroll top and anim

--- src/comp/BlogPost.js
+++ src/comp/BlogPost.js
@@ -1,12 +1,17 @@
-import React from "react";
-
+import React, {useEffect} from "react";
+import ScrollAnimation from 'react-animate-on-scroll'
 export default function BlogPost(props) {
-    console.log(props);
+    useEffect(() => {
+        document.documentElement.style = "scroll-behavior: auto";
+        document.documentElement.scrollTo({ top: 0, behavior: "auto" }); // or just leave it window.scrollTo(...), doens't matter. I just like it this way ;)
+    }, []);
     return (
+        <ScrollAnimation animateIn="fadeIn" offset={250} duration={0.75}>
         <div className="blog-post">
             <h1>{props.post.title}</h1>
             <hr />
             {props.post.content}
         </div>
+        </ScrollAnimation>
     );
 }
