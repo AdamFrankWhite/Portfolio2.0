916163a31794113b5cd513c55b808b18a5df9c62
Adam White <adamfrankwhite@gmail.com>
2020-10-23 13:31:19 +0100

    changed to snapshot, smoother and faster load

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import { hydrate, render } from "react-dom";
 import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-} else {
-    render(<App />, rootElement);
-}
+import { render } from "react-snapshot";
+render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
