97263da37f6d14183e09a063bb485b8178b0bfc2
Adam White <adamfrankwhite@gmail.com>
2020-12-28 16:12:40 +0000

    footer redesign

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -7,28 +7,39 @@ export default function Footer() {
     return (
         <footer>
             <div className="footer-content">
-                <div>
+                <div className="footer-about">
                     <h3>Adam White</h3>
-                    <h4>Freelance Web Developer</h4>
+                    <h4>Web Designer and Developer</h4>
                     <hr />
-                    <a
-                        href="https://www.linkedin.com/in/adam-white-781aa2168"
-                        rel="noopener"
-                        target="_blank"
-                    >
-                        <FontAwesomeIcon icon={faLinkedin} />
-                    </a>
-                    <a href="mailto:info@adamwhite.tech" rel="noopener">
-                        <FontAwesomeIcon icon={faEnvelopeOpen} />
-                    </a>
+                    <p>
+                        Freelance web designer and developer based in Stockport,
+                        UK. I'm passionate about all things web design and am
+                        here to help small businesses improve their web
+                        presence.
+                    </p>
                 </div>
-                {/* <div>
-                    <div className="link-cont">
-                        <Link to="/privacy-policy">Privacy Policy</Link>
-                        |<p>Working With Me</p>
-                    </div> 
-                    
-                </div> */}
+
+                <div className="link-cont">
+                    <div className="row-1">
+                        <a
+                            href="https://www.linkedin.com/in/adam-white-781aa2168"
+                            rel="noopener"
+                            target="_blank"
+                        >
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href="mailto:info@adamwhite.tech" rel="noopener">
+                            <FontAwesomeIcon
+                                icon={faEnvelopeOpen}
+                                style={{ padding: "0.05em" }}
+                            />
+                        </a>
+                    </div>
+                    <div className="row-2">
+                        <Link to="/privacy-policy">Privacy Policy</Link>|
+                        <a>Working With Me</a>
+                    </div>
+                </div>
             </div>
         </footer>
     );
